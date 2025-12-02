8a4368deef740bc999aa5a82a766bc08b7a30520
Mageh1 <marion.gehin.mg@gmail.com>
2025-12-02 15:27:53 +0100

    Add files via upload

--- 3RRR.py
+++ 3RRR.py
@@ -6,15 +6,15 @@ ADDR_MX_TORQUE_ENABLE = 24
 ADDR_MX_GOAL_POSITION = 30
 ADDR_MX_MOVING_SPEED = 32
 PROTOCOL_VERSION = 1.0
-h = 0.07 # taille coté triangle equi 5cm
+h = 0.07 # taille coté triangle equi 7cm
 L = 0.1 # longueur des bras 10cm
 R = 0.275/2  # rayon du cercle des servomoteurs
 angles_moteurs = np.radians([0, 120, 240])
 Ox = R*np.cos(angles_moteurs) #les trois points contenant les servomoteurs
 Oy = R*np.sin(angles_moteurs)
 
-DEVICE_NAME = "/dev/ttyUSB0"
-BAUDRATE = 57600
+DEVICE_NAME = "COM5"
+BAUDRATE = 1000000
 
 DXL_IDS = [1,2,3]
 
@@ -22,7 +22,7 @@ portHandler = PortHandler(DEVICE_NAME)
 packetHandler = PacketHandler(PROTOCOL_VERSION)
 
 def degres_to_moteur(degres): 
-    return ((degres / 360) *4096)
+    return int((degres / 360) *4096)
 
 def enable_torque(dxl_id):
     packetHandler.write1ByteTxRx(portHandler, dxl_id, ADDR_MX_TORQUE_ENABLE, 1)
@@ -36,9 +36,8 @@ def set_speed(dxl_id, speed):
 def move_motor(dxl_id, tick):
     packetHandler.write2ByteTxRx(portHandler, dxl_id, ADDR_MX_GOAL_POSITION, tick) #tick de 0 à 4096
 
-def initialisation(angle_pos_ini):
-    angle_pos_ini_deg = np.degrees(angle_pos_ini)
-    tick = degres_to_moteur(angle_pos_ini_deg)
+def initialisation(angle_pos):
+    tick = degres_to_moteur(angle_pos)
 
     for dxl_id in DXL_IDS:
         enable_torque(dxl_id)
@@ -72,11 +71,11 @@ def inverse_kinematics(x,y):
         angle_beta[i] = np.arccos(cos_beta)
 
         angle_alpha[i] = np.arctan2(dy,dx) - np.arctan2(L*np.sin(angle_beta[i]),L+ L*np.cos(angle_beta[i]))
-        print("angle alpha : %f " , angle_alpha[i] )
-        print("angle beta : %f " , angle_beta[i] )
-        print("xC, yC, %f , %f", xC[i], yC[i])
+        print("angle alpha :  " , angle_alpha[i] )
+        print("angle beta :  " , angle_beta[i] )
+        print("xC, yC", xC[i], yC[i])
         i = i + 1
-    return angle_alpha
+    return np.degrees(angle_alpha)
 
 def positionsB(alpha):
     Bx = []
@@ -102,34 +101,48 @@ def mouvement_vers(x,y):
         return False
 
     #tick_angleA = degres_to_moteur(angle_a)
-    i = 0 
-    while(i<3):
-        tick_angleA = degres_to_moteur(angle_a[i])
-        move_motor(DXL_IDS[i],tick_angleA[i])
-        i = i + 1
+    #i = 1 # vu que les ids vont de 1 à 3.
+    for i in range(3):
+        tick = degres_to_moteur(angle_a[i])
+        move_motor(DXL_IDS[i], tick)
+        #tick_angleA = degres_to_moteur(angle_a[i-1])
+        #move_motor(DXL_IDS[i],int(tick_angleA[i]))
+        #i = i + 1
     return ("Mouvement vers (%f,%f)",x,y)
 
-def main(): 
-    i = 0
-    path = [] 
-    while(i < 10):
-        path.append((0,i*0.02)) # une ligne droite la du coup
-        i = i+1
-    
-    initialisation(0.31655754) #valeur des angles pour etre au centre askip.
 
-    for pos in path : 
-        mouvement_vers(pos)
-        print("pos : %f", pos)
+def main(): 
+    if not portHandler.openPort():
+        print("ERROR: Impossible d'ouvrir le port série.")
+        quit()
+
+    if not portHandler.setBaudRate(BAUDRATE):
+        print("ERROR: Impossible de régler le baudrate.")
+        quit()
+
+    print("lancement du code")
+    initialisation(110)
+    print("parpitié)")
+    # i = 0
+    # trace_x, trace_y = [], []
+    # target_points = []
+    # while(i < 125):
+    #     target_x = 0.00 + 0.02 * np.cos(i * 0.05) # Réduire le rayon à 2cm
+    #     target_y = 0.00 + 0.02 * np.sin(i * 0.05)
+    #     target_points.append((target_x, target_y))
+    #     i += 1
+    #     i += 1
+    
+    # initialisation(110.0) #valeur des angles pour etre au centre (déduis grace à dynamixel)
+
+    # for x, y in target_points : 
+    #     mouvement_vers(x,y)
+    #     print("pos :", x,y)
 
     for dxl in DXL_IDS:
         disable_torque(dxl)
 
     portHandler.closePort()
+    print("fin du code") 
 
-
-
-
-
-
-
+main()
